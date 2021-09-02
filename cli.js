8f1cbb51cc74464487eef789e8b9f7e7e90346a3
Hernán Cervera <hjcerveram@hotmail.com>
2021-09-02 10:31:56 -0500

    Initial commit

---
+++ cli.js
@@ -0,0 +1,5 @@
+#!/usr/bin/env node
+
+const greet = require("./index");
+
+greet(process.argv[2]);
---
+++ index.js
@@ -0,0 +1,5 @@
+function greet(name) {
+  console.log(`I salute you ${name}`);
+}
+
+module.exports = greet;
